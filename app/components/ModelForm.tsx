8436104bf82eba01310a6d8636048844d0ef55d9
Luke Elabor <lukee@globalkinetic.com>
2025-02-03 08:26:51 +0200

    refactor: allowed ModelForm to have metadata

--- app/components/ModelForm.tsx
+++ app/components/ModelForm.tsx
@@ -1,26 +1,60 @@
 import { FC } from "react";
 
+interface IMetadata {
+  name: string;
+  question: string;
+  type: string;
+  domain?: { values?: string[]; upper?: number; lower?: number };
+}
+
 interface IProps {
   inputs: Record<string, string>;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  metadata: IMetadata[];
+  onChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => void;
   onSubmit: (e: React.FormEvent) => void;
 }
 
-export const ModelForm: FC<IProps> = ({ inputs, onChange, onSubmit }) => (
+export const ModelForm: FC<IProps> = ({
+  inputs,
+  metadata,
+  onChange,
+  onSubmit,
+}) => (
   <div className="space-y-2">
-    {Object.keys(inputs).map((key) => (
-      <div key={key}>
-        <label className="block">{key}</label>
-        <input
-          type="text"
-          name={key}
-          value={inputs[key]}
-          onChange={onChange}
-          className="w-full p-2 border rounded"
-        />
+    {metadata.map(({ name, question, type, domain }) => (
+      <div key={name}>
+        <label className="block">{question}</label>
+        {type === "Nominal" && domain?.values ? (
+          <select
+            name={name}
+            value={inputs[name]}
+            onChange={onChange}
+            className="w-full p-2 border rounded"
+          >
+            <option value="">Select an option</option>
+            {domain.values.map((option: string) => (
+              <option key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        ) : (
+          <input
+            type="number"
+            name={name}
+            value={inputs[name]}
+            onChange={onChange}
+            min={domain?.lower ?? ""}
+            max={domain?.upper ?? ""}
+            className="w-full p-2 border rounded"
+          />
+        )}
       </div>
     ))}
     <button
+      type="button"
       onClick={onSubmit}
       className="w-full py-2 bg-blue-500 text-white rounded-md"
     >
